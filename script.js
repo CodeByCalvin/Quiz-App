b8d37fde41299fd922ff10fc2a348e98a8637fec
Calvin Merry <calvin@calvinmerry.com>
2023-05-15 17:06:47 +0100

    Restructured my quizQuestions object and wrote a function to display the current question and update the UI

--- script.js
+++ script.js
@@ -1,86 +1,116 @@
 const quizQuestions = [
   {
-    question1: {
-      question: "What is your preferred game genre?",
-      answerA: "MMORPG (Massively Multiplayer Online Role-Playing Game)",
-      answerB: "Racing",
-      answerC: "First-person Shooter",
-      answerD: "Action Role-Playing",
-    },
-    question2: {
-      question: "How do you feel about the graphics of a game?",
-      answerA: "I prefer retro-style graphics.",
-      answerB: "I enjoy colorful and cartoonish graphics.",
-      answerC: "I like realistic and modern graphics.",
-      answerD: "I like immersive, fantasy-styled graphics.",
-    },
-    question3: {
-      question: "How do you prefer to play games?",
-      answerA: "I like to immerse myself in a world with other players.",
-      answerB: "I prefer fast-paced competitive games with friends.",
-      answerC: "I like to compete against others online in intense matches.",
-      answerD: "I prefer a single-player experience with a rich storyline.",
-    },
-    question4: {
-      question: "What's your ideal game pace?",
-      answerA: "Slow and steady, allowing me to grind at my own pace.",
-      answerB: "Fast and frantic, requiring quick reflexes.",
-      answerC: "Moderate pace with strategic planning.",
-      answerD: "Slow-paced with lots of exploration and world-building.",
-    },
-    question5: {
-      question: "What kind of setting do you prefer in a game?",
-      answerA: "A vast world with different cities, guilds, and economies.",
-      answerB: "Fun and whimsical tracks with vibrant environments.",
-      answerC: "Modern and realistic war zones.",
-      answerD: "A vast fantasy world with various landscapes and creatures.",
-    },
-    question6: {
-      question: "What level of difficulty do you enjoy in a game?",
-      answerA:
-        "I enjoy a game that can be easy or hard based on the paths I choose.",
-      answerB: "I prefer a game that's easy to learn but hard to master.",
-      answerC: "I enjoy high-stakes, challenging games.",
-      answerD:
-        "I prefer games that are challenging due to their complexity and depth.",
-    },
-    question7: {
-      question: "What kind of game progression do you prefer?",
-      answerA:
-        "I like games where progress is largely determined by the time invested.",
-      answerB: "I prefer games with a series of levels or stages to complete.",
-      answerC: "I enjoy games where progression is tied to skill and strategy.",
-      answerD:
-        "I like games where progression is linked to exploration and quest completion.",
-    },
-    question8: {
-      question: "How much time do you want to invest in a game?",
-      answerA:
-        "I'm ready to commit hundreds of hours to live my alternate life.",
-      answerB: "I want something I can pick up and play in short bursts.",
-      answerC:
-        "I'm willing to invest time in mastering the game mechanics and strategies.",
-      answerD:
-        "I want a long, epic journey that I can complete at my own pace.",
-    },
-    question9: {
-      question: "What kind of reward system do you prefer in a game?",
-      answerA: "I like games where I can trade and build wealth.",
-      answerB: "I prefer games that reward me with faster cars and new tracks.",
-      answerC: "I like games that reward precision and tactical skills.",
-      answerD:
-        "I enjoy games that reward exploration and quest completion with new items and abilities.",
-    },
-    question10: {
-      question: "What level of interaction do you prefer with other players?",
-      answerA:
-        "I enjoy a game where I can interact with a large online community.",
-      answerB:
-        "I like games where I can compete with friends in the same room.",
-      answerC:
-        "I enjoy games where I can team up or compete with others online.",
-      answerD:
-        "I prefer games where I can play independently and focus on my own journey.",
-    },
+    question: "What is your preferred game genre?",
+    answerA: "MMORPG",
+    answerB: "Racing",
+    answerC: "First-person Shooter",
+    answerD: "Action Role-Playing",
+  },
+  {
+    question: "How do you feel about the graphics of a game?",
+    answerA: "I prefer retro-style graphics.",
+    answerB: "I enjoy colorful and cartoonish graphics.",
+    answerC: "I like realistic and modern graphics.",
+    answerD: "I like immersive, fantasy-styled graphics.",
+  },
+  {
+    question: "How do you prefer to play games?",
+    answerA: "I like to immerse myself in a world with other players.",
+    answerB: "I prefer fast-paced competitive games with friends.",
+    answerC: "I like to compete against others online in intense matches.",
+    answerD: "I prefer a single-player experience with a rich storyline.",
+  },
+  {
+    question: "What's your ideal game pace?",
+    answerA: "Slow and steady, allowing me to grind at my own pace.",
+    answerB: "Fast and frantic, requiring quick reflexes.",
+    answerC: "Moderate pace with strategic planning.",
+    answerD: "Slow-paced with lots of exploration and world-building.",
+  },
+  {
+    question: "What kind of setting do you prefer in a game?",
+    answerA: "A vast world with different cities, guilds, and economies.",
+    answerB: "Fun and whimsical tracks with vibrant environments.",
+    answerC: "Modern and realistic war zones.",
+    answerD: "A vast fantasy world with various landscapes and creatures.",
+  },
+  {
+    question: "What level of difficulty do you enjoy in a game?",
+    answerA:
+      "I enjoy a game that can be easy or hard based on the paths I choose.",
+    answerB: "I prefer a game that's easy to learn but hard to master.",
+    answerC: "I enjoy high-stakes, challenging games.",
+    answerD:
+      "I prefer games that are challenging due to their complexity and depth.",
+  },
+  {
+    question: "What kind of game progression do you prefer?",
+    answerA:
+      "I like games where progress is largely determined by the time invested.",
+    answerB: "I prefer games with a series of levels or stages to complete.",
+    answerC: "I enjoy games where progression is tied to skill and strategy.",
+    answerD:
+      "I like games where progression is linked to exploration and quest completion.",
+  },
+  {
+    question: "How much time do you want to invest in a game?",
+    answerA: "I'm ready to commit hundreds of hours to live my alternate life.",
+    answerB: "I want something I can pick up and play in short bursts.",
+    answerC:
+      "I'm willing to invest time in mastering the game mechanics and strategies.",
+    answerD: "I want a long, epic journey that I can complete at my own pace.",
+  },
+  {
+    question: "What kind of reward system do you prefer in a game?",
+    answerA: "I like games where I can trade and build wealth.",
+    answerB: "I prefer games that reward me with faster cars and new tracks.",
+    answerC: "I like games that reward precision and tactical skills.",
+    answerD:
+      "I enjoy games that reward exploration and quest completion with new items and abilities.",
+  },
+  {
+    question: "What level of interaction do you prefer with other players?",
+    answerA:
+      "I enjoy a game where I can interact with a large online community.",
+    answerB: "I like games where I can compete with friends in the same room.",
+    answerC: "I enjoy games where I can team up or compete with others online.",
+    answerD:
+      "I prefer games where I can play independently and focus on my own journey.",
   },
 ];
+
+/// Selecting elements
+const questionTitle = document.querySelector(".question-title");
+const questionText = document.querySelector(".question-text");
+const answers = document.querySelectorAll(".answer");
+
+const answerAText = document.querySelector(".text-a");
+const answerBText = document.querySelector(".text-b");
+const answerCText = document.querySelector(".text-c");
+const answerDText = document.querySelector(".text-d");
+
+const questionNumber = document.querySelector(".question-number");
+const progressBar = document.querySelector(".progress-bar");
+const progressBarActive = document.querySelector(".progress-bar-active");
+
+let currentQuestion = 0;
+
+// Functions
+function displayQuestion() {
+  questionTitle.innerHTML = quizQuestions[currentQuestion].question;
+  answerAText.innerHTML = quizQuestions[currentQuestion].answerA;
+  answerBText.innerHTML = quizQuestions[currentQuestion].answerB;
+  answerCText.innerHTML = quizQuestions[currentQuestion].answerC;
+  answerDText.innerHTML = quizQuestions[currentQuestion].answerD;
+  questionNumber.innerHTML = `${currentQuestion + 1} / ${quizQuestions.length}`;
+  progressBarActive.style.width = `${currentQuestion * 10}%`;
+}
+
+displayQuestion();
+
+answers.forEach((answer) => {
+  answer.addEventListener("click", () => {
+    currentQuestion++;
+    displayQuestion();
+  });
+});
