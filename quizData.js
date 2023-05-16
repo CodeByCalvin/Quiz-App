79e291c6942ecac037a3b3a8c141340a3f694bcc
Calvin Merry <calvin@calvinmerry.com>
2023-05-16 14:20:57 +0100

    Created a separate js file called quizData.js

---
+++ quizData.js
@@ -0,0 +1,115 @@
+/////////////////////////////// STORED DATA ///////////////////////////////
+
+export const quizQuestions = [
+  {
+    question: "What is your preferred game genre?",
+    answerA: "MMORPG",
+    answerB: "Racing",
+    answerC: "First-person Shooter",
+    answerD: "Action Role-Playing",
+  },
+  {
+    question: "How do you feel about the graphics of a game?",
+    answerA: "I prefer retro-style graphics.",
+    answerB: "I enjoy colorful and cartoonish graphics.",
+    answerC: "I like realistic and modern graphics.",
+    answerD: "I like immersive, fantasy-styled graphics.",
+  },
+  {
+    question: "How do you prefer to play games?",
+    answerA: "I like to immerse myself in a world with other players.",
+    answerB: "I prefer fast-paced competitive games with friends.",
+    answerC: "I like to compete against others online in intense matches.",
+    answerD: "I prefer a single-player experience with a rich storyline.",
+  },
+  {
+    question: "What's your ideal game pace?",
+    answerA: "Slow and steady, allowing me to grind at my own pace.",
+    answerB: "Fast and frantic, requiring quick reflexes.",
+    answerC: "Moderate pace with strategic planning.",
+    answerD: "Slow-paced with lots of exploration and world-building.",
+  },
+  {
+    question: "What kind of setting do you prefer in a game?",
+    answerA: "A vast world with different cities, guilds, and economies.",
+    answerB: "Fun and whimsical tracks with vibrant environments.",
+    answerC: "Modern and realistic war zones.",
+    answerD: "A vast fantasy world with various landscapes and creatures.",
+  },
+  {
+    question: "What level of difficulty do you enjoy in a game?",
+    answerA:
+      "I enjoy a game that can be easy or hard based on the paths I choose.",
+    answerB: "I prefer a game that's easy to learn but hard to master.",
+    answerC: "I enjoy high-stakes, challenging games.",
+    answerD:
+      "I prefer games that are challenging due to their complexity and depth.",
+  },
+  {
+    question: "What kind of game progression do you prefer?",
+    answerA:
+      "I like games where progress is largely determined by the time invested.",
+    answerB: "I prefer games with a series of levels or stages to complete.",
+    answerC: "I enjoy games where progression is tied to skill and strategy.",
+    answerD:
+      "I like games where progression is linked to exploration and quest completion.",
+  },
+  {
+    question: "How much time do you want to invest in a game?",
+    answerA: "I'm ready to commit hundreds of hours to live my alternate life.",
+    answerB: "I want something I can pick up and play in short bursts.",
+    answerC:
+      "I'm willing to invest time in mastering the game mechanics and strategies.",
+    answerD: "I want a long, epic journey that I can complete at my own pace.",
+  },
+  {
+    question: "What kind of reward system do you prefer in a game?",
+    answerA: "I like games where I can trade and build wealth.",
+    answerB: "I prefer games that reward me with faster cars and new tracks.",
+    answerC: "I like games that reward precision and tactical skills.",
+    answerD:
+      "I enjoy games that reward exploration and quest completion with new items and abilities.",
+  },
+  {
+    question: "What level of interaction do you prefer with other players?",
+    answerA:
+      "I enjoy a game where I can interact with a large online community.",
+    answerB: "I like games where I can compete with friends in the same room.",
+    answerC: "I enjoy games where I can team up or compete with others online.",
+    answerD:
+      "I prefer games where I can play independently and focus on my own journey.",
+  },
+];
+
+export const quizResults = [
+  {
+    result: "MMORPG",
+    description:
+      "You enjoy immersing yourself in a vast world with other players. You like to build your character and progress through the game at your own pace. You enjoy the social aspect of the game and interacting with other players.",
+    image: "imgs/OSRSlogo.jpeg",
+    game: "OldSchool Runescape",
+  },
+
+  {
+    result: "Racing",
+    description:
+      "You enjoy fast-paced, competitive games with friends. You like to compete against others in intense matches. You enjoy the thrill of racing and the challenge of mastering the tracks.",
+    image: "imgs/MarioKartLogo.jpeg",
+    game: "Mario Kart",
+  },
+
+  {
+    result: "First-person Shooter",
+    description:
+      "You enjoy competitive games with fast-paced action. You like to compete against others online in intense matches. You enjoy the thrill of combat and the challenge of mastering the weapons.",
+    image: "imgs/CallOfDutyLogo.jpeg",
+    game: "Call of Duty",
+  },
+  {
+    result: "Action Role-Playing",
+    description:
+      "You enjoy a single-player experience with a rich storyline. You like to immerse yourself in a fantasy world with various landscapes and creatures. You enjoy the thrill of exploration and the challenge of completing quests.",
+    image: "imgs/SkyrimLogo.jpeg",
+    game: "Skyrim",
+  },
+];
