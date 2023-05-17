cfbb8c76cf735b5ceb85e32ce9e33033aea78a1a
Calvin Merry <calvin@calvinmerry.com>
2023-05-17 11:55:46 +0100

    Refactored code, organised and created some helper functions to reduce repetitive code

--- script.js
+++ script.js
@@ -2,27 +2,31 @@ import { quizQuestions, quizResults } from "./quizData.js";
 
 /////////////////////////////// SELECTING ELEMENTS ///////////////////////////////
 
+// Screen elements
 const startScreen = document.querySelector(".start-screen");
-const startBtn = document.querySelector(".start-btn");
-
-const questionTitle = document.querySelector(".question-title");
-const questionText = document.querySelector(".question-text");
 const questionBox = document.querySelector(".question-box");
+const loadingScreen = document.querySelector(".loading-screen");
+const resultsScreen = document.querySelector(".results-screen");
+
+// Button elements
+const startBtn = document.querySelector(".start-btn");
+const restartBtn = document.querySelector(".restart-btn");
 const answers = document.querySelectorAll(".answer");
+
+// Question elements
+const questionTitle = document.querySelector(".question-title");
 const answerAText = document.querySelector(".text-a");
 const answerBText = document.querySelector(".text-b");
 const answerCText = document.querySelector(".text-c");
 const answerDText = document.querySelector(".text-d");
 
-const loadingScreen = document.querySelector(".loading-screen");
-
-const resultsScreen = document.querySelector(".results-screen");
+// Result elements
 const resultTitle = document.querySelector(".result-title");
 const resultsDescription = document.querySelector(".results-description");
 const resultsGame = document.querySelector(".results-game");
 const resultsImg = document.querySelector(".results-img");
-const restartBtn = document.querySelector(".restart-btn");
 
+// Progress elements
 const questionNumber = document.querySelector(".question-number");
 const progressBar = document.querySelector(".progress-bar");
 const progressBarActive = document.querySelector(".progress-bar-active");
@@ -33,40 +37,53 @@ let gameState = {
   userAnswers: [],
 };
 
-/////////////////////////////// START QUIZ ///////////////////////////////
+/////////////////////////////// HELPER FUNCTIONS ///////////////////////////////
 
 // Reset game state and display start screen
 function resetGameState() {
   gameState.currentQuestion = 0;
   gameState.userAnswers = [];
-  startScreen.classList.remove("hide");
-  questionBox.classList.add("hide");
-  resultsScreen.classList.add("hide");
 }
+
+// Show a specific screen (and hide the rest)
+function showScreen(screen) {
+  hideScreen(startScreen);
+  hideScreen(questionBox);
+  hideScreen(resultsScreen);
+  screen.classList.remove("hide");
+}
+
+// Hides a specific screen
+function hideScreen(screen) {
+  screen.classList.add("hide");
+}
+
+/////////////////////////////// START QUIZ ///////////////////////////////
+
+function startQuiz() {
+  hideScreen(startScreen);
+  showScreen(questionBox);
+  displayQuestion();
+}
+
+// Start button
+startBtn.addEventListener("click", () => {
+  setTimeout(() => {
+    startBtn.classList.add("fade-out");
+    startQuiz();
+  }, 500);
+});
 
 function updateDisplay() {
   resetGameState();
   if (gameState.currentQuestion > 0 && gameState.currentQuestion < 10) {
-    startScreen.classList.add("hide");
-    questionBox.classList.remove("hide");
+    showScreen(questionBox);
   } else if (gameState.currentQuestion === 10) {
-    startScreen.classList.add("hide");
-    questionBox.classList.add("hide");
-    resultsScreen.classList.remove("hide");
+    showScreen(resultsScreen);
+  } else {
+    showScreen(startScreen);
   }
 }
-
-updateDisplay();
-
-// Start button
-startBtn.addEventListener("click", () => {
-  startBtn.classList.add("fade-out");
-  setTimeout(() => {
-    startScreen.classList.add("hide");
-    questionBox.classList.remove("hide");
-    displayQuestion();
-  }, 500);
-});
 
 // Display question
 function displayQuestion() {
@@ -83,7 +100,7 @@ function displayQuestion() {
   questionNumber.innerHTML = `${gameState.currentQuestion + 1} / ${
     quizQuestions.length
   }`;
-  progressBarActive.style.width = `${(gameState.currentQuestion + 1) * 10}%`;
+  progressBarActive.style.width = `${gameState.currentQuestion * 10}%`;
 
   // Remove fade-in class after 1 second
   setTimeout(() => {
@@ -97,7 +114,7 @@ answers.forEach((answer) => {
   answer.addEventListener("click", (e) => {
     gameState.currentQuestion++;
     let answerLetter = e.currentTarget.getAttribute("answer");
-    userAnswers.push(answerLetter);
+    gameState.userAnswers.push(answerLetter);
     if (
       gameState.currentQuestion < quizQuestions.length &&
       gameState.currentQuestion < 10
@@ -124,16 +141,13 @@ function showLoadingScreen() {
 
 // Displaying the results
 function displayResults() {
-  //////////// Hide question box
-  questionBox.classList.add("hide");
-
   //////////// Calculate the result
   function calculateResults() {
     let counts = { A: 0, B: 0, C: 0, D: 0 };
 
     // Count the frequency of each answer
-    for (let i = 0; i < userAnswers.length; i++) {
-      let answer = userAnswers[i];
+    for (let i = 0; i < gameState.userAnswers.length; i++) {
+      let answer = gameState.userAnswers[i];
       if (counts[answer]) {
         counts[answer]++;
       } else {
